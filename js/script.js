3af0f6d7a1723758449bc4f0f879e101512f2fd6
matheus-fraga <matheuscarlosfraga@gmail.com>
2023-07-27 15:26:26 -0300

    alteração no script.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ import { temaCSS } from "./tema_css.js";
 import { temaHTML } from "./tema_html.js";
 import { temaJavaScript } from "./tema_js.js";
 import { initMusicPlayer } from './musicPlayer.js';
+import {nomeUsuario} from './user_name_storage.js';
+import {toggleTheme, initToggleTheme} from './toggleTheme.js';
 initMusicPlayer();
+initToggleTheme();

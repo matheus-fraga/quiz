69d5bb586f745fd59d521857868d359c15493d45
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-07-27 14:46:04 -0300

    criado funções da tabela de resultados

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import { temaCSS } from "./tema_css.js";
 import { temaHTML } from "./tema_html.js";
 import { temaJavaScript } from "./tema_js.js";
+import { showResults } from "./results.js";
 import { initMusicPlayer } from './musicPlayer.js';
-initMusicPlayer();
+// initMusicPlayer();
+showResults();

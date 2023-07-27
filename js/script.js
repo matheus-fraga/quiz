af36d1b2aa1adc5993623249f92ab48ef0d3d66f
matheus-fraga <matheuscarlosfraga@gmail.com>
2023-07-27 18:43:56 -0300

    ultimas alterações

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-// import { temaCSS } from "./tema_css.js";
-// import { temaHTML } from "./tema_html.js";
-// import { temaJavaScript } from "./tema_js.js";
+import { temaCSS } from "./tema_css.js";
+import { temaHTML } from "./tema_html.js";
+import { temaJavaScript } from "./tema_js.js";
 import { initMusicPlayer } from './musicPlayer.js';
-import {toggleTheme, initToggleTheme} from './toggleTheme.js';
-import { showResults } from "./results.js";
 import { findTopFive, showResults } from "./results.js";
 import { showQuestions } from './showQuestions.js';
 import {nomeUsuario} from './user_name_storage.js';
+import {toggleTheme, initToggleTheme} from './toggleTheme.js';
 
-// initMusicPlayer();
+initMusicPlayer();
 initToggleTheme();
-// showQuestions("CSS");
+showQuestions("HTML");
 showResults();
 findTopFive();
 

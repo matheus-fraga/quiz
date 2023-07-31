6997bff405e3530101c8dad8f36926925504f932
quilantera <gugaquilante@gmail.com>
2023-07-31 14:06:02 -0300

    adicionando a funcao de desabilitar formulario

--- js/verifyQuestions.js
+++ js/verifyQuestions.js
@@ -1,58 +1,81 @@
-// Função para verificar quais são as respostas corretas, e acumular os pontos gerados pelo usuário
+
 export function verifyQuestions(theme) {
+
+    //Referenciando os componentes HTML
+    const doneButton = document.querySelector(".done");
+    const restartButton = document.querySelector(".restart");
+    const nextPageButton = document.querySelector(".next");
+    const questionForm = document.querySelector(".questionary-form");
+    const questionContainers = document.querySelectorAll(".question-container");
+
+    //contador para as respostas concluidas
+    let respondedQuestionsCount = 0;
+    //contador total de acertos 
     let totalPoints = 0;
-
-
-    const submitQuestionary = document.querySelector(".done");
-    const returnPage = document.querySelector(".restart");
-    const nextPage = document.querySelector(".next");
-
-    submitQuestionary.addEventListener("click", function (event) {
+    //funcao do botao concluir questionario.
+    doneButton.addEventListener("click", function (event) {
         event.preventDefault();
-        const form = document.querySelector(".questionary-form");
-        const questions = document.querySelectorAll(".question-container");
-        const formData = new FormData(form);
-        let isDone = true;
-        questions.forEach((question, index) => {
 
-            const checkedValue = formData.get(`question${index}`);
+        const formData = new FormData(questionForm);
+        questionContainers.forEach((question, index) => {
+            
+            const correctAnswer = theme[index].answer; // pega a posição da resposta correta referenciado no objeto
+            const checkedValue = formData.get(`question${index}`); // pega o valor da resposta inserida pelo usuário
 
-            if (checkedValue == theme[index].answer) {
-                question.classList.add("success");
-                question.classList.remove("wrong");
-                question.classList.remove("warning");
-                totalPoints++;
-            } else if (checkedValue == undefined) {
+            if (checkedValue == correctAnswer) {
+                handleCorrectAnswer(question, index);
+                totalPoints++; //soma os pontos do usuário
+            } else if (!checkedValue) {
                 question.classList.add("warning");
-                isDone = false;
             } else {
-                question.classList.add("wrong");
-                question.classList.remove("success");
-                question.classList.remove("warning");
+                handleWrongAnswer(question, index);
+            }
+
+            if (respondedQuestionsCount === questionContainers.length) {
+                enableNextPage();
             }
         });
-        if (isDone) {
-            submitQuestionary.style.display = "none";
-            returnPage.style.display = "none";
-            nextPage.style.display = "block";
-        }
     });
 
-    nextPage.addEventListener("click", function () {
+    nextPageButton.addEventListener("click", function () {
+       console.log("Gustavo",'CSS','2:00',getDateQuestionary(),totalPoints,totalPoints,totalPoints)
+       //  results(name, bestTheme, time, dateQuiz, scoreHTML, scoreCSS, scoreJavaScript)
+    });
+
+    //funcao que muda o campo como a resposta correta
+    function handleCorrectAnswer(question, index) {
+        question.classList.add("success");
+        respondedQuestionsCount++;
+        disableQuestionInputs(index, question);
+    }
+    //funcao que muda o campo como a resposta errada
+    function handleWrongAnswer(question, index) {
+        question.classList.add("wrong");
+        respondedQuestionsCount++;
+        disableQuestionInputs(index, question);
+    }
+    //funcao que retira os botoes de resetar e concluir e adicionar o seguir.
+    function enableNextPage() { 
+        doneButton.style.display = "none";
+        restartButton.style.display = "none";
+        nextPageButton.style.display = "block";
+    }
+
+    function disableQuestionInputs(index, question) {
+        const questionInputs = question.querySelectorAll(`input[name="question${index}"]`);
+        questionInputs.forEach(input => {
+            input.disabled = true;
+
+        });
+    }
+
+    function getDateQuestionary(){
         const dateSubmit = new Date();
         const day = dateSubmit.getDate();
-        const month = dateSubmit.getMonth() + 1; // Os meses são indexados a partir de 0, por isso adicionamos 1.
+        const month = dateSubmit.getMonth() + 1;
         const year = dateSubmit.getFullYear();
-        const dateConcat = `${day}/${month}/${year}`;
-        console.log(
-            "carrega: " +
-            "Gustavo " +
-            "tema" +
-            "CSS" +
-            "data" +
-            dateConcat +
-            "nota: " +
-            totalPoints
-        );
-    });
+
+        return(`${day}/${month}/${year}`);
+    }
+    
 }
